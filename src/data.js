2b6a53f7c9de8ace2466efca9aa589a4ff7a7a60
J. Matthew Pryor <jmp@tenacious.ventures>
2023-02-14 14:41:07 +1100

    initial version

--- src/data.js
+++ src/data.js
@@ -1,14 +1,9 @@
 // Skills icons - https://react-icons.github.io/react-icons/
 import {
-  FaHtml5,
-  FaCss3Alt,
-  FaBootstrap,
   FaReact,
-  FaGitAlt,
-  FaGithubSquare,
   FaLink,
 } from "react-icons/fa";
-import { SiJavascript, SiStyledcomponents, SiRedux } from "react-icons/si";
+import { SiJavascript } from "react-icons/si";
 
 // Projects Images (add your images to the images directory and import below)
 import Logo from "./images/logo.svg";
@@ -24,7 +19,7 @@ export { HeroDark as Dark };
  **************************************************************
   Add your GitHub username (string - "YourUsername") below.
 */
-export const githubUsername = "Your GitHub username here";
+export const githubUsername = "jmatthewpryor";
 
 // Blog link icon (imported above)
 export const Blog = <FaLink />;
@@ -34,7 +29,7 @@ export const Blog = <FaLink />;
   Add a second paragraph for the about me section.
 */
 export const moreInfo =
-  "I enjoy learning about technology and helping others use it to improve their lives and be more productive. I built this site with React, React Bootstrap, Redux, and the GitHub REST API.";
+  "I am a venture investor, and am passionate about the role of technology in scaling up climate solutions in the agr-food value chain. Sometimes I dabble in a bit of technology myself to help make my work more productive.";
 
 /* Skills
  ************************************************************** 
@@ -42,49 +37,14 @@ export const moreInfo =
 */
 export const skillData = [
   {
-    id: 1,
-    skill: <FaHtml5 className="display-4" />,
-    name: "HTML5",
-  },
-  {
-    id: 2,
-    skill: <FaCss3Alt className="display-4" />,
-    name: "CSS3",
-  },
-  {
     id: 3,
     skill: <SiJavascript className="display-4" />,
     name: "JavaScript",
   },
   {
-    id: 4,
-    skill: <FaBootstrap className="display-4" />,
-    name: "BootStrap",
-  },
-  {
     id: 5,
     skill: <FaReact className="display-4" />,
     name: "React",
-  },
-  {
-    id: 6,
-    skill: <SiStyledcomponents className="display-4" />,
-    name: "Styled Components",
-  },
-  {
-    id: 7,
-    skill: <SiRedux className="display-4" />,
-    name: "Redux",
-  },
-  {
-    id: 8,
-    skill: <FaGitAlt className="display-4" />,
-    name: "Git",
-  },
-  {
-    id: 9,
-    skill: <FaGithubSquare className="display-4" />,
-    name: "GitHub",
   },
 ];
 
@@ -95,7 +55,7 @@ export const resume = null;
  ************************************************************** 
   List the repo names (string - "your-repo-name") you want to include (they will be sorted alphabetically). If empty, only the first 3 will be included.
 */
-export const filteredProjects = ["example-1", "example-2", "example-3"];
+export const filteredProjects = ["raycast-google-cal-util"];
 
 // Replace the defualt GitHub image for matching repos below (images imported above - lines 13-14)
 export const projectCardImages = [
@@ -110,4 +70,4 @@ export const projectCardImages = [
   Add your formspree endpoint below.
   https://formspree.io/
 */
-export const formspreeUrl = "https://formspree.io/f/YourEndpoint";
+export const formspreeUrl = "https://formspree.io/f/mayzevlw";
